20fb4ff42507b442eca542b7c63f928754a7008c
Vasyl Kazimirov <kazimirov.v@gmail.com>
2025-11-03 10:46:26 +0100

    update config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -171,7 +171,7 @@ return {
         csharp_ls = {},
         rust_analyzer = {},
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
